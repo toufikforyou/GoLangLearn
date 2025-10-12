a92ca143ed10e8976bcbe0b01393bfcbda278d1a
MH TOUFIK <toufikforyou@gmail.com>
2025-10-12 11:35:39 +0600

    Add simple Go program to print greetings

---
+++ 2_simple_values/simple.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// // int print
+	// fmt.Println(12)
+	// fmt.Println(1 + 10)
+	// fmt.Println(2 * 34)
+	// fmt.Println(2 / 1)
+	// fmt.Println(4 ^ 2)
+
+	// // float print
+	// fmt.Println(12.44444)
+
+	// // bool print
+	// fmt.Println(false)
+	// fmt.Println(true)
+
+	// string print
+	fmt.Println("Hello World")
+	fmt.Println("Hello!")
+
+}
