44cb797dc66b9f42473917db1fed62293cb1a33e
MH TOUFIK <toufikforyou@gmail.com>
2025-10-12 11:37:34 +0600

    Add variables demonstration program in Go

---
+++ 3_variables/variables.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "MH TOUFIK"
+	var name2 = "GO LANG"
+	name3 := "TOUFIK FOR YOU"
+
+	fmt.Println(name)
+	fmt.Println(name2)
+	fmt.Println(name3)
+
+	var isValid bool = true
+	isAdult := false
+
+	fmt.Println(isValid)
+	fmt.Println(isAdult)
+
+	floatValue := 3.1416
+
+	fmt.Println(floatValue)
+
+	var age int16
+	age = 12
+
+	fmt.Println(age)
+
+	var price float32 = 50.12
+	price = 12.323
+
+	fmt.Println(price)
+}
